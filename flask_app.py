44de5a6762369e48e47c4e5ecf47806c15c9c62b
bertilipsen <ipsen.bertil@gmail.com>
2019-09-05 15:34:54 +0000

    getting templates to work

--- flask_app.py
+++ flask_app.py
@@ -17,7 +17,7 @@ project_dir = os.path.dirname(os.path.realpath(__file__))
 database_file = "sqlite:///{}".format(
     os.path.join(project_dir, "offerdatabase.db"))
 
-app = Flask(__name__, template_folder='/static/')
+app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
 
